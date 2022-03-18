35d9e2eab0a11f1f28f0bd2272d4dde0cf01deda
beniamin_savu <beniamin_savu@sync.ro>
2022-03-18 13:59:24 +0200

    Fixed build

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,7 +1,39 @@
+import { JSDOM } from 'jsdom';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+// https://github.com/enzymejs/enzyme/blob/master/docs/guides/jsdom.md
+const jsdom = new JSDOM(
+    '<!doctype html><html><body></body></html>',
+    {url: "https://localhost/"}
+);
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+global.requestAnimationFrame = function (callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function (id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
+
+
+
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
+
 // Make Enzyme functions available in all test files without importing
 global.shallow = shallow;
 global.render = render;
